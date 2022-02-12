fe9bd08359e78cda8dc3ed8deb5504a599c5ec68
mateusaffonso <augustomateus@poli.ufrj.br>
2022-02-12 01:39:41 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    amigos: [
+      {
+        nome: "Douglas",
+        idade: 26,
+        profissao: "Ajudante de farmácia"
+      },
+      {
+        nome: "Gabriela",
+        idade: 26,
+        profissao: "Arquiteta"
+      },
+      {
+        nome: "Nathalia",
+        idade: 22,
+        profissao: "psicóloga"
+      },
+      {
+        nome: "Jhonatan",
+        idade: 26,
+        profissao: "Vendedor"
+      },
+      {
+        nome: "Eloana",
+        idade: 37,
+        profissao: "Assistente Social"
+      },
+      {
+        nome: "Vinicius",
+        idade: 39,
+        profissao: "Professor"
+      },
+      {
+        nome: "Julia",
+        idade: 18,
+        profissao: "Jovem Aprendiz"
+      }
+      ]
+  }
+
+  render() {
+
+    return (
+
+      <div className="cardFather">
+        <h1>Informações sobre alguns amigos</h1>
+        {this.state.amigos.map((item) => (
+          <div className="card">
+
+            <p><strong>Nome: </strong>{item.nome}</p>
+            <p><strong>idade: </strong>{item.idade} anos</p>
+            <p><strong>Profissão: </strong>{item.profissao}</p>
+            
+          </div>
+        ))}
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
